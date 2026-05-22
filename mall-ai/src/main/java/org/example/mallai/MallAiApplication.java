0d80be0dc8d7f56f2d52ccb0ceb5ec2899c200b9
jackzhen237 <2373343995@qq.com>
2026-05-23 01:17:14 +0800

    最终版集成ai

--- mall-ai/src/main/java/org/example/mallai/MallAiApplication.java
+++ mall-ai/src/main/java/org/example/mallai/MallAiApplication.java
@@ -2,8 +2,12 @@ package org.example.mallai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {"org.example"})
+@EnableFeignClients
 public class MallAiApplication {
 
     public static void main(String[] args) {
